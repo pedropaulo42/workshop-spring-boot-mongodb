9d8ef713cbbd75822ec4409fdadc0412d50c2ee4
Pedro Paulo <pedroppaulo42@gmail.com>
2020-10-21 21:57:11 -0300

    Acrescentando comentários aos posts

--- src/main/java/com/pedropaulo/workshopmongo/domain/Post.java
+++ src/main/java/com/pedropaulo/workshopmongo/domain/Post.java
@@ -1,11 +1,14 @@
 package com.pedropaulo.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.pedropaulo.workshopmongo.dto.AuthorDTO;
+import com.pedropaulo.workshopmongo.dto.CommentDTO;
 
 @Document(collection="post")
 public class Post implements Serializable {
@@ -18,6 +21,7 @@ public class Post implements Serializable {
 	private String body;
 	
 	private AuthorDTO author;
+	private List<CommentDTO> comments = new ArrayList<>();
 	
 	public Post() {
 	}
@@ -69,6 +73,14 @@ public class Post implements Serializable {
 
 	public void setAuthor(AuthorDTO author) {
 		this.author = author;
+	}
+	
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
 	}
 
 	@Override
